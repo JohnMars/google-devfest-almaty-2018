6ade543c5fc05513f9b61dfaa0221a568fac533d
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:51:02 +0600

    TASK: Создание AdvertDetailsViewModelFactory
    Реализация метода Factory#create

--- app/src/main/java/kz/kolesa/devfest/advertdetails/AdvertDetailsViewModelFactory.kt
+++ app/src/main/java/kz/kolesa/devfest/advertdetails/AdvertDetailsViewModelFactory.kt
@@ -1,7 +1,19 @@
 package kz.kolesa.devfest.advertdetails
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class AdvertDetailsViewModelFactory(
         private val advertisementId: Long
-) : ViewModelProvider.Factory
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        @Suppress("UNCHECKED_CAST")
+        if (modelClass.isAssignableFrom(AdvertDetailsViewModel::class.java)) {
+            return AdvertDetailsViewModel(advertisementId) as T
+        }
+
+        throw IllegalArgumentException("Could not instantiate " +
+                AdvertDetailsViewModel::class.java.simpleName)
+    }
+}
