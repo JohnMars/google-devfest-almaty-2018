fd440656b638823688b1322cded15dad28e716fa
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:08:37 +0600

    TASK: Обновление данных LiveData в действии
    Добавить сервис

--- app/src/main/java/kz/kolesa/devfest/advertlist/AdvertListViewModel.kt
+++ app/src/main/java/kz/kolesa/devfest/advertlist/AdvertListViewModel.kt
@@ -3,9 +3,15 @@ package kz.kolesa.devfest.advertlist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kz.kolesa.devfest.data.api.ADVERTISEMENT_SERVICE
+import kz.kolesa.devfest.data.api.AdvertisementService
+import kz.kolesa.devfest.data.mapper.ApiAdvertisementMapper
 import kz.kolesa.devfest.domain.entity.Advertisement
 
-class AdvertListViewModel: ViewModel() {
+class AdvertListViewModel(
+        private val advertisementService: AdvertisementService = ADVERTISEMENT_SERVICE,
+        private val apiAdvertisementMapper: ApiAdvertisementMapper = ApiAdvertisementMapper()
+) : ViewModel() {
 
     private val advertListLiveData = MutableLiveData<List<Advertisement>>()
 
