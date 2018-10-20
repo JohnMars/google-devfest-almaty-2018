2445a0a22f88f677ffe0d36562e47af6a7421a87
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:47:20 +0600

    TASK: Реализация AdvertDetailsViewModel
    Создание LiveData

--- app/src/main/java/kz/kolesa/devfest/advertdetails/AdvertDetailsViewModel.kt
+++ app/src/main/java/kz/kolesa/devfest/advertdetails/AdvertDetailsViewModel.kt
@@ -1,9 +1,15 @@
 package kz.kolesa.devfest.advertdetails
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kz.kolesa.devfest.domain.entity.Advertisement
+
 /**
  * ViewModel для экрана объявления
  */
-class AdvertDetailsViewModel {
+class AdvertDetailsViewModel: ViewModel() {
+
+    val advertisementLiveData = MutableLiveData<Advertisement>()
 
     fun onCallClicked() {
         TODO("Вытащить номер телефон из advertisementLiveData")
