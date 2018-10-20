df125e1694b0534d239985068180a3c46ecfc569
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:29:37 +0600

    TASK: Создать базу данных
    Инициализация KolesaDatabase

--- app/src/main/java/kz/kolesa/devfest/KolesaApplication.kt
+++ app/src/main/java/kz/kolesa/devfest/KolesaApplication.kt
@@ -1,11 +1,12 @@
 package kz.kolesa.devfest
 
 import android.app.Application
+import kz.kolesa.devfest.data.room.KolesaDatabase
 
 class KolesaApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        TODO("Инициализировать KolesaDatabase")
+        KolesaDatabase.initialize(this)
     }
 }
