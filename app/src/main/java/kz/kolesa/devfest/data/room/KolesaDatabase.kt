c49481b038ebc65c9e889673dca5139474c24a08
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:31:21 +0600

    TASK: Добавить компоненты TypeConverter
    Добавить аннотацию TypeConverters

--- app/src/main/java/kz/kolesa/devfest/data/room/KolesaDatabase.kt
+++ app/src/main/java/kz/kolesa/devfest/data/room/KolesaDatabase.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
         entities = [RoomAdvertisement::class],
         version = 1
 )
+@TypeConverters
 abstract class KolesaDatabase : RoomDatabase() {
 
     companion object {
