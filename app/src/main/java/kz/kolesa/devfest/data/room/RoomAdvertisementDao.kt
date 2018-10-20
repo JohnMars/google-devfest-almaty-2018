69ce379e610aa4259aee21003aa8d31127dffd70
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:20:18 +0600

    TASK: Создать Dao
    Добавить метод getAll c аннотацием

--- app/src/main/java/kz/kolesa/devfest/data/room/RoomAdvertisementDao.kt
+++ app/src/main/java/kz/kolesa/devfest/data/room/RoomAdvertisementDao.kt
@@ -3,9 +3,13 @@ package kz.kolesa.devfest.data.room
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface RoomAdvertisementDao {
+
+    @Query("SELECT * FROM advertisements")
+    fun getAll(): List<RoomAdvertisement>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg advertisement: RoomAdvertisement)
