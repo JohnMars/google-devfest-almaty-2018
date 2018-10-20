c3aee312088923582da3ccfd289758fe1590329b
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:49:09 +0600

    TASK: Реализация AdvertDetailsViewModel
    Запрос объявления

--- app/src/main/java/kz/kolesa/devfest/advertdetails/AdvertDetailsViewModel.kt
+++ app/src/main/java/kz/kolesa/devfest/advertdetails/AdvertDetailsViewModel.kt
@@ -2,20 +2,38 @@ package kz.kolesa.devfest.advertdetails
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.experimental.DefaultDispatcher
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.withContext
+import kz.kolesa.devfest.data.DEFAULT_ADVERTISEMENT_REPOSITORY
 import kz.kolesa.devfest.domain.entity.Advertisement
+import kz.kolesa.devfest.domain.repository.AdvertisementRepository
 
 /**
  * ViewModel для экрана объявления
  */
-class AdvertDetailsViewModel: ViewModel() {
+class AdvertDetailsViewModel(
+        private val advertisementId: Long,
+        private val advertisementRepository: AdvertisementRepository = DEFAULT_ADVERTISEMENT_REPOSITORY
+): ViewModel() {
 
-    val advertisementLiveData = MutableLiveData<Advertisement>()
+    val advertisementLiveData = MutableLiveData<Advertisement>().apply {
+        if (value == null) {
+            requestAdvertisement(advertisementId)
+        }
+    }
 
     fun onCallClicked() {
         TODO("Вытащить номер телефон из advertisementLiveData")
     }
 
     private fun requestAdvertisement(advertisementId: Long) {
-        TODO("Загрузить объявление из Repository")
+        launch(UI) {
+            val advertisement = withContext(DefaultDispatcher) {
+                advertisementRepository.getAdvertisement(advertisementId)
+            }
+            advertisementLiveData.value = advertisement
+        }
     }
 }
