7d8830ec51817d7bdc3e23fe5d29fa670ff70600
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:19:25 +0600

    TASK: Создать Dao
    Добавить аннотацию Insert

--- app/src/main/java/kz/kolesa/devfest/data/room/RoomAdvertisementDao.kt
+++ app/src/main/java/kz/kolesa/devfest/data/room/RoomAdvertisementDao.kt
@@ -1,9 +1,11 @@
 package kz.kolesa.devfest.data.room
 
 import androidx.room.Dao
+import androidx.room.Insert
 
 @Dao
 interface RoomAdvertisementDao {
 
+    @Insert()
     fun insertAll(vararg advertisement: RoomAdvertisement)
 }
