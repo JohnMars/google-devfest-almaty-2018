cd49443dcc961ff3a5dc7860097f4edf10da86b1
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 12:02:16 +0600

    Добавил файл AdvertDetailsViewModel

---
+++ app/src/main/java/kz/kolesa/devfest/advertdetails/AdvertDetailsViewModel.kt
@@ -0,0 +1,15 @@
+package kz.kolesa.devfest.advertdetails
+
+/**
+ * ViewModel для экрана объявления
+ */
+class AdvertDetailsViewModel {
+
+    fun onCallClicked() {
+        TODO("Вытащить номер телефон из advertisementLiveData")
+    }
+
+    private fun requestAdvertisement(advertisementId: Long) {
+        TODO("Загрузить объявление из Repository")
+    }
+}
