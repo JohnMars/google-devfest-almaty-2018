8c5b4e978fe69b9dbbaf846b34a068ff0c552f58
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:23:08 +0600

    TASK: Создать базу данных
    Указать аннотацию Database

--- app/src/main/java/kz/kolesa/devfest/data/room/KolesaDatabase.kt
+++ app/src/main/java/kz/kolesa/devfest/data/room/KolesaDatabase.kt
@@ -1,7 +1,9 @@
 package kz.kolesa.devfest.data.room
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
 
 const val KOLESA_DATABASE_NAME = "kolesa"
 
+@Database()
 abstract class KolesaDatabase : RoomDatabase()
