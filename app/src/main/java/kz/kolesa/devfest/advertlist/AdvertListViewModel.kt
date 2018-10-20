8587d370b7eda11f3528015f6d9e2e73bbaa9944
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:03:08 +0600

    TASK: Создание LiveData
    Поменять метод getAdvertisements на LiveData

--- app/src/main/java/kz/kolesa/devfest/advertlist/AdvertListViewModel.kt
+++ app/src/main/java/kz/kolesa/devfest/advertlist/AdvertListViewModel.kt
@@ -1,5 +1,6 @@
 package kz.kolesa.devfest.advertlist
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kz.kolesa.devfest.domain.entity.Advertisement
@@ -7,4 +8,6 @@ import kz.kolesa.devfest.domain.entity.Advertisement
 class AdvertListViewModel: ViewModel() {
 
     private val advertListLiveData = MutableLiveData<List<Advertisement>>()
+
+    fun getAdvertisements(): LiveData<List<Advertisement>> = advertListLiveData
 }
