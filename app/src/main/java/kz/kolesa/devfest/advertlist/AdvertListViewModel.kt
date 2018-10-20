bc10ef8920d072b2039bb75aee88259dd9a43991
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:01:25 +0600

    TASK: Создание LiveData
    Добавил переменную LiveData

--- app/src/main/java/kz/kolesa/devfest/advertlist/AdvertListViewModel.kt
+++ app/src/main/java/kz/kolesa/devfest/advertlist/AdvertListViewModel.kt
@@ -1,9 +1,10 @@
 package kz.kolesa.devfest.advertlist
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kz.kolesa.devfest.domain.entity.Advertisement
 
 class AdvertListViewModel: ViewModel() {
 
-    fun getAdvertisements(): List<Advertisement> = emptyList()
+    private val advertListLiveData = MutableLiveData<List<Advertisement>>()
 }
