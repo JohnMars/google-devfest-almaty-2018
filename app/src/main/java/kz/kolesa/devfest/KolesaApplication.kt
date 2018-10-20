3459bd754b12360d98148ed2c67a0b8baf06965b
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 11:55:25 +0600

    KolesaApplication

---
+++ app/src/main/java/kz/kolesa/devfest/KolesaApplication.kt
@@ -0,0 +1,11 @@
+package kz.kolesa.devfest
+
+import android.app.Application
+
+class KolesaApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        TODO("Инициализировать KolesaDatabase")
+    }
+}
