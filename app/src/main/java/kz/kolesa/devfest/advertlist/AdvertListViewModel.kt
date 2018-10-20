9c1a158b5ddd7eb53b2da256d4b6ba120faad23f
Janibek Marshal <marshal@kolesa.kz>
2018-10-20 14:11:31 +0600

    TASK: Обновление данных LiveData в действии
    Реализация асинхронной задачи

--- app/src/main/java/kz/kolesa/devfest/advertlist/AdvertListViewModel.kt
+++ app/src/main/java/kz/kolesa/devfest/advertlist/AdvertListViewModel.kt
@@ -3,6 +3,10 @@ package kz.kolesa.devfest.advertlist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.experimental.DefaultDispatcher
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.withContext
 import kz.kolesa.devfest.data.api.ADVERTISEMENT_SERVICE
 import kz.kolesa.devfest.data.api.AdvertisementService
 import kz.kolesa.devfest.data.mapper.ApiAdvertisementMapper
@@ -17,7 +21,17 @@ class AdvertListViewModel(
 
     fun getAdvertisements(): LiveData<List<Advertisement>> = advertListLiveData.apply {
         if (value == null) {
-            TODO("Загрузка данных")
+            requestAdvertisements()
+        }
+    }
+
+    private fun requestAdvertisements() {
+        launch(UI) {
+            val advertisements = withContext(DefaultDispatcher) {
+                val searchResponse = advertisementService.searchAdvertisements().execute()
+                searchResponse.body()?.map { apiAdvertisementMapper.map(it) }
+            }
+            TODO("Уведомляем LiveData")
         }
     }
 }
